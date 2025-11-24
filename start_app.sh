714a89783043066c513943be0f32ab71dd1733ac
Bidit das <110318738+bidit06@users.noreply.github.com>
2025-11-24 06:52:45 +0000

    Day3 work done

--- start_app.sh
+++ start_app.sh
@@ -2,7 +2,8 @@
 
 # Start all services in background
 livekit-server --dev &
-(cd backend && uv run python src/agent_barista.py dev) &
+# To run any other agent, just change the file name or location below here:
+(cd backend && uv run python src/agent_wellness_log dev) &
 (cd frontend && pnpm dev) &
 
 # Wait for all background jobs
